2adde306e3fb5d490aa88fe495c441827b81b08a
jamalraouj <jamalrwj@gmail.com>
2022-05-22 14:35:44 +0100

    I add class InfoController for inheritance

---
+++ app/controllers/InfoController.php
@@ -0,0 +1,11 @@
+<?php  
+
+   abstract class InfoController extends Controller{
+
+        private $nom_complet;
+        private $genre;
+        private $matricule;
+        private $img_profile;
+    }
+
+?>
--- app/models/Professor.php
+++ app/models/Professor.php
@@ -1,6 +1,6 @@
 <?php 
     class Professor{
-
+        
         private $db;
         public function __construct( )    {
         $this->db = new Database();
